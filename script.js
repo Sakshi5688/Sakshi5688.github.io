a1ca4e90a40ed254c7744f7ac14172b5c9d293c2
Sakshi5688 <chavansakshi568@gmail.com>
2026-09-13 16:31:50 +0530

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,49 @@
+// ==========================================
+// NAVIGATION ACTIVE LINK
+// ==========================================
+
+const sections = document.querySelectorAll("section");
+
+const navLinks = document.querySelectorAll("nav a");
+
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 200;
+
+        const sectionHeight = section.clientHeight;
+
+        if (
+            window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight
+        ) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+
+    
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+
+        if (
+            link.getAttribute("href") === "#" + current
+        ) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
